5380924e36bc855a0b88e9b519a0fe9c1ef4466d
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2023-11-24 20:21:29 +0200

    Modify code for multiple linear regression

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -1,60 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+import pylab
 
 a = 0.001
-m = 3
-size = np.array([2104, 1600, 2400, 1416, 1534, 852, 3000])
+iter = 100
+size = np.array([2.1, 1.6, 2.4, 1.4, 1.5, 0.8, 3])  # kinda work
 num_of_bedrooms = np.array([3, 3, 3, 2, 3, 2, 4])
-ones = np.ones(shape=len(size), dtype=int)
-
-# combine 'ones', 'size' and 'num_of_bedrooms' into pairs into one 2D array
-input_x = np.vstack((ones, size, num_of_bedrooms)).T
-
 y = np.array([400, 330, 369, 232, 315, 178, 540])
-params = np.zeros(shape=m, dtype=object)
+n = 3
+input_x = np.vstack((np.ones(shape=len(size), dtype=int), size, num_of_bedrooms)).T
+m = len(input_x)
+params = np.zeros(shape=n, dtype=object)
+
+"""
+a - learning rate
+iter - iterations
+x - inputs
+y - target values
+n - # of features
+input_x - matrix containing all training examples
+m - # of rows in input_X
+params - parameters(θ1, θ2,..., θn)
+"""
 
 
 def train():
-    pass
-
-
-# def train():
-#     global params
-#     for i in range(len(input_x)):
-#         for j in range(m):
-#             hpths = hypothesis(input_x[i])
-#             # visualize_hypothesis(input_x[j][1], hpths)
-#             params[j] -= a * (hpths - y[i]) * input_x[i][j]
-
-
-def cost_func(x):
-    return 1/2 * np.matmul(hypothesis(x) - y, hypothesis(x) - y)
-
-# def cost_func(x):
-#     return np.matmul(hypothesis(x) - y, hypothesis(x) - y)
+    global params
+    for i in range(m):
+        for j in range(n):
+            hpths = hypothesis(input_x[i])
+            params[j] -= a * (hpths - y[i]) * input_x[i][j]
 
 
 def hypothesis(x):
-    return np.matmul(params, x)
+    return np.matmul(x, params)
 
 
-def visualize_hypothesis(_x, _y):
-    plt.plot(_x, _y)
-    plt.show()
+def cost_func():
+    return 1/2 * np.matmul(hypothesis(input_x) - y, hypothesis(input_x) - y)
 
 
 def visualize_training_examples():
-    plt.scatter(input_x[:, 1], y)
-    plt.xlabel('Area (feet^2)')
+    plt.plot(input_x[:, 1], y, 'x')
+    plt.xlabel('Area (1000 feet^2)')
     plt.ylabel('Price ($1000s)')
     plt.show()
 
 
-if __name__ == '__main__':
-    # visualize_training_examples()
-    print(params)
+def visualize_cost(cost):
+    plt.plot(cost)
+    plt.show()
 
-    for i in range(10):
+
+if __name__ == '__main__':
+    visualize_training_examples()
+    cost_hist = []
+
+    for i in range(iter):
+        cost_hist.append(cost_func())
         train()
-        print(params)
+
+    print(params)
+    visualize_cost(cost_hist)
