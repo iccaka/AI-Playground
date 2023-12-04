17d7ca7a501face235093da9b32b906b4fdc25c0
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2023-12-04 03:43:07 +0200

    WIP - Modify neural_network/main.py

--- neural_network/main.py
+++ neural_network/main.py
@@ -1,38 +1,59 @@
 import numpy as np
-import torch
-import torch.nn.functional as F
+import random
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+m = 200
+iter = 1000
+random.seed(222)
+x1 = np.array([random.uniform(175, 260) for _ in range(m)])
+x2 = np.array([random.uniform(12, 15) for _ in range(m)])
+y = np.array([random.randrange(0, 2) for _ in range(m)])
+X = np.vstack((x1, x2)).T
+n = X.shape[1]
+W1 = np.array([[-8.93,  0.29, 12.9], [-0.1,  -7.32, 10.81]])
+b1 = np.array([-9.82, -9.28,  0.96])
+W2 = np.array([[-31.18], [-27.59], [-32.56]])
+b2 = np.array([15.41])
+# W = np.zeros(n)
+# b = 0
 
 
-def get_rand_values():
-    return torch.randn(3)
+def sequential(a0, W1, b1, W2, b2, _dense):
+    a1 = _dense(a0, W1, b2)
+    return _dense(a1, W2, b2)
 
 
-def mean_squared_error(predictions, targets):
-    return ((predictions - targets)**2).mean()
+def dense(a, _W, _b):
+    return hypothesis(a, _W, _b)
 
 
-def f(t, params):
-    a, b, c = params
-    return a*(t**2) + (b*t) + c
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def hypothesis(w, x, b):
+    return sigmoid(np.dot(w, x) + b)
+
+
+def visualize_training_examples():
+    colors = ['red' if i == 1 else 'blue' for i in y]
+
+    # fig, ax = plt.subplots()
+    plt.scatter(x1, x2, marker='x', c=colors)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.show()
 
 
 if __name__ == '__main__':
-    t = torch.arange(0, 20).float()
-    speed = torch.randn(20) * 3 + 0.75 * (t - 9.5) ** 2 + 1
-    # params = get_rand_values().requires_grad_()
-    # targets = get_rand_values()
-    #
-    # # pass params to func and get predictions
-    # predictions = f(t, params)
-    # print(predictions.shape, targets.shape)
-    #
-    # loss = mean_squared_error(predictions, targets)
-    # loss.backward()
-    # print(predictions.grad)
-    #
-    # learning_rate = 1e-5
-    # predictions.data -= learning_rate * predictions.grad.data
-    # predictions.grad = None
-    #
-    # predictions = f(t, params)
-    # print(mean_squared_error(predictions, targets))
+    # visualize_training_examples()
+
+    norm = tf.keras.layers.Normalization(axis=-1)
+    norm.adapt(X)
+    X_norm = norm(X)
+
+    p = np.zeros((m, 1))
+    for i in range(m):
+        p[i, 0] = sequential(X_norm[i], W1, b1, W2, b2, dense)
+    print(p)
