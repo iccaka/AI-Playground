719090f24cb13f22551a85fba9cf114848df2e50
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2024-07-09 00:34:32 +0300

    Update the summary, build and fit methods

--- neural_network/model.py
+++ neural_network/model.py
@@ -14,15 +14,28 @@ class Model:
                 layer.name = 'layer_{}'.format(str(i + 1))
 
     def summary(self):
-        # TODO output weights' shape
         # TODO add weight count for each layer
         # TODO add total weights count
-        for layer in self.layers:
-            print(
-                'Name:', layer.name,
-                ' / Units:', len(layer.units),
-                '/ Activation:', layer.activation.__name__
-            )
+        if self.are_weights_initialized:
+            weights = self.get_weights()
+
+            for i in range(0, len(weights), 2):
+                layer = self.layers[int(i / 2)]
+
+                print('Name: {} / Units: {} / Activation: {}\n\tw: {} / b: {}'.format(
+                    layer.name,
+                    layer.unit_count,
+                    layer.activation.__name__,
+                    weights[i].shape,
+                    weights[i + 1].shape
+                ))
+        else:
+            for layer in self.layers:
+                print('Name: {} / Units: {} / Activation: {}'.format(
+                    layer.name,
+                    layer.unit_count,
+                    layer.activation.__name__
+                ))
 
     def get_layer(self, name=None, position=None):
         if position is None and name is None:
@@ -41,7 +54,7 @@ class Model:
             raise ValueError('No layer with such name found:', name)
 
     def get_weights(self):
-        # TODO make it using np.zeros and fill it
+        # TODO make it using np.zeros and fill it?
         weights = []
 
         for layer in self.layers:
@@ -59,16 +72,18 @@ class Model:
             b = weights[i + 1]
             self.layers[int(i / 2)].set_weights(w, b)
 
-    def build(self, input_shape, xavier_init=False):
+    def build(self, input_shape, initializer='xavier'):
+        # TODO add xavier and he initialization
+
         w_1 = np.random.randn(self.layers[0].unit_count, input_shape[0])
         # TODO second dimension for b_1 = 1 instead of this?
-        b_1 = np.random.randn(self.layers[0].unit_count, )
+        b_1 = np.random.randn(self.layers[0].unit_count, 1)
         self.layers[0].set_weights(w_1, b_1)
 
         for i, layer in enumerate(self.layers[1:], start=0):
             w_i = np.random.randn(layer.unit_count, self.layers[i].unit_count)
             # TODO second dimension for b_i = 1 instead of this?
-            b_i = np.random.randn(layer.unit_count, )
+            b_i = np.random.randn(layer.unit_count, 1)
             layer.set_weights(w_i, b_i)
 
         self.are_weights_initialized = True
@@ -80,7 +95,15 @@ class Model:
         if not self.are_weights_initialized:
             self.build(x.shape)
 
-        # TODO forward prop
+        A = x
+        # TODO forward prop in a separate function
+        # TODO add cache for use during back prop
         # TODO back prop
-        for layer in range(len(self.layers)):
-            pass
+        for layer in self.layers:
+            # TODO check if input dimensions match for forward prop
+            Z = Layer.linear_transform(*layer.get_weights(), A)
+            A = layer.activation(Z)
+
+    # TODO finish predict
+    def predict(self):
+        pass
