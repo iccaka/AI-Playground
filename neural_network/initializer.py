e784d64eeb62d6e10fbd67b50084a718737b484a
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2024-09-22 04:12:47 +0300

    Modify initializer.py

--- neural_network/initializer.py
+++ neural_network/initializer.py
@@ -7,43 +7,38 @@ class Initializer:
         return np.random.randn(*shape), np.random.randn(shape[0], 1)
 
     @staticmethod
-    def xavier_uni(shape, **kwargs):
-        # TODO check whether n_out and n_in are passed or not
-        if 'n_in' not in kwargs:
-            raise ValueError('')
-        if 'n_out' not in kwargs:
-            raise ValueError('')
+    def xavier_uni(shape):
+        limit = np.sqrt(6 / (shape[0] + shape[1]))
 
-        # W_i = np.random.uniform(
-        #     low=-(np.sqrt(6 / (input_shape[1] + self.layers[0].unit_count))),
-        #     high=np.sqrt(6 / (input_shape[1] + self.layers[0].unit_count)),
-        #     size=(self.layers[0].unit_count, input_shape[1])
-        # )
-        # b_i = np.zeros(shape=(self.layers[0].unit_count, 1))
-        # self.layers[0].set_weights(W_i, b_i)
-        #
-        # for i, layer in enumerate(self.layers[1:], start=0):
-        #     W_i = np.random.uniform(
-        #         low=-(np.sqrt(6 / (self.layers[i].unit_count + layer.unit_count))),
-        #         high=np.sqrt(6 / (self.layers[i].unit_count + layer.unit_count)),
-        #         size=(layer.unit_count, self.layers[i].unit_count)
-        #     )
-        #     b_i = np.zeros(shape=(layer.unit_count, 1))
-        #     layer.set_weights(W_i, b_i)
+        # TODO should anything be done with b(np.zeros...)?
         return np.random.uniform(
-            low=-(np.sqrt(6 / (kwargs['n_in'] + kwargs['n_out']))),
-            high=np.sqrt(6 / (kwargs['n_in'] + kwargs['n_out'])),
+            low=-limit,
+            high=limit,
             size=shape
         ), np.zeros(shape=(shape[0], 1))
 
     @staticmethod
-    def he_uni(shape, **kwargs):
-        pass
+    def he_uni(shape):
+        limit = np.sqrt(6 / shape[1])
+
+        return np.random.uniform(
+            low=-limit,
+            high=limit,
+            size=shape
+        ), np.zeros(shape=(shape[0], 1))
 
     @staticmethod
-    def xavier_norm(shape, **kwargs):
-        pass
+    def xavier_norm(shape):
+        return np.random.normal(
+            loc=0,
+            scale=np.sqrt(2 / (shape[0] + shape[1])),
+            size=shape
+        ), np.zeros(shape=(shape[0], 1))
 
     @staticmethod
-    def he_norm(shape, **kwargs):
-        pass
+    def he_norm(shape):
+        return np.random.normal(
+            loc=0,
+            scale=np.sqrt(2 / shape[1]),
+            size=shape
+        ), np.zeros(shape=(shape[0], 1))
